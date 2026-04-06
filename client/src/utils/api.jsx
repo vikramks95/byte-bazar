1c65cd7e59e61e7289fec8477f84bb5e3102d410
vikramks95 <vikramks091@gmail.com>
2026-04-06 23:09:00 +0530

    Update FE Env

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: apiUrl || 'http://localhost:5000/api',
 });
 
 // Attach token to every request
